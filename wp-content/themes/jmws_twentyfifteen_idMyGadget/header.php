c6c9611dd0a744f6a0e5483bc63ec8319c42ed40
Tom W Hartung <tomwhartung@gmail.com>
2015-08-08 22:28:18 -0600

    Updated meta viewport tag.

--- wp-content/themes/jmws_twentyfifteen_idMyGadget/header.php
+++ wp-content/themes/jmws_twentyfifteen_idMyGadget/header.php
@@ -12,7 +12,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!--[if lt IE 9]>
