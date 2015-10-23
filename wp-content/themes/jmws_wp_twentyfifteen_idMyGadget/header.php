ad3145d389d4074e87320fd6c10be1e4e770f69e
Tom W Hartung <tomwhartung@gmail.com>
2015-10-23 13:22:10 -0600

    Minor cleanup.

--- wp-content/themes/jmws_wp_twentyfifteen_idMyGadget/header.php
+++ wp-content/themes/jmws_wp_twentyfifteen_idMyGadget/header.php
@@ -76,7 +76,7 @@ $header_html .= '</header><!-- .site-header -->';
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site" <?php echo $jmwsIdMyGadget->jqmDataRole['page'] ?>>
 	<?php if( $jmwsIdMyGadget->phoneHeaderNavIn2015Page ) : ?>
-		<nav data-role="navbar" id="page-phone-nav">
+		<nav data-role="navbar">
 			<?php wp_nav_menu( array('theme_location' => 'phone-header-nav', 'container' => false) ); ?>
 		</nav>
 	<?php endif; ?>
@@ -86,7 +86,7 @@ $header_html .= '</header><!-- .site-header -->';
 	<div id="sidebar" class="sidebar">
 
 		<?php if( $jmwsIdMyGadget->phoneHeaderNavIn2015Sidebar ) : ?>
-			<nav data-role="navbar" id="sidebar-phone-nav">
+			<nav data-role="navbar">
 				<?php wp_nav_menu( array('theme_location' => 'phone-header-nav', 'container' => false) ); ?>
 			</nav>
 		<?php endif; ?>
