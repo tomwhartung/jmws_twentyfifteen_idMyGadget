755c915eac1d8ebc823f714c5fe7e7a9782787e9
Tom W Hartung <tomwhartung@gmail.com>
2015-11-10 09:32:39 -0700

    Updated header.php to call the new getLogoTitleDescriptionHtml function.

--- wp-content/themes/jmws_wp_twentyfifteen_idMyGadget/header.php
+++ wp-content/themes/jmws_wp_twentyfifteen_idMyGadget/header.php
@@ -38,30 +38,11 @@ $logoTitleDescription = '';
 //	if ( FALSE )
 if ( $jmwsIdMyGadget->isInstalled() && $jmwsIdMyGadget->isEnabled() )
 {
-  $logoTitleDescription = $jmwsIdMyGadget->getLogoTitleDescriptionHtml();
+	$logoTitleDescription = $jmwsIdMyGadget->getLogoTitleDescriptionHtml();
 }
 else
 {
-  // Device detection is not available so we use this,
-  // which is the original twentyfifteen code (as of Sept. 2015).
-  //
-  if ( is_front_page() && is_home() )
-  {
-    $logoTitleDescription = '<h1 class="site-title">' .
-      '<a href="' . esc_url( home_url('/') ) . '" rel="home">' . $site_name . '</a></h1>';
-  }
-  else
-  {
-    $logoTitleDescription = '<p class="site-title">' .
-      '<a href="' . esc_url( home_url('/') ) . '" rel="home">' . $site_name . '</a></p>';
-  }
-  $description = get_bloginfo( 'description', 'display' );
-  if ( $description || is_customize_preview() )
-  {
-    $logoTitleDescription .= '<p class="site-description">' . $description . '</p>';
-  }
-  // $logoTitleDescription .= '<button class="secondary-toggle">' . _e( 'Menu and widgets', 'twentyfifteen' ) . '</button>';
-  $logoTitleDescription .= '<button class="secondary-toggle">Menu and widgets</button>';
+	$logoTitleDescription = getLogoTitleDescriptionHtml();
 }
 $header_html = '';
 $header_html .= '<header id="masthead" class="site-header" role="banner" ';
