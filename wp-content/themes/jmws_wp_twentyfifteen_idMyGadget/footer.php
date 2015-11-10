aa6e687b01b74438fe4bc962be6bdd45a223bc14
Tom W Hartung <tomwhartung@gmail.com>
2015-11-10 09:31:33 -0700

    Commented out call to get the sanity check string.

--- wp-content/themes/jmws_wp_twentyfifteen_idMyGadget/footer.php
+++ wp-content/themes/jmws_wp_twentyfifteen_idMyGadget/footer.php
@@ -27,7 +27,7 @@ global $jmwsIdMyGadget;
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
 		</div><!-- .site-info -->
 		<div class="debug">
-			<?php print $jmwsIdMyGadget->getSanityCheckString(); ?>
+			<?php // print $jmwsIdMyGadget->getSanityCheckString(); ?>
 		</div><!-- .debug -->
 	</footer><!-- .site-footer -->
 
