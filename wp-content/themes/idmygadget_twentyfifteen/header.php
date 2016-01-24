a1d0a0c2b8b2f925dafd344dce6638c7cda133b5
Tom W Hartung <tomwhartung@gmail.com>
2016-01-24 15:24:35 -0700

    Moved a require_once statement and the check for idmygadget installation from header.php to functions.php .

--- wp-content/themes/idmygadget_twentyfifteen/header.php
+++ wp-content/themes/idmygadget_twentyfifteen/header.php
@@ -31,8 +31,6 @@
 //   Call its fcn to get the html we need for the header
 //
 global $jmwsIdMyGadget;
-require_once 'idMyGadget/JmwsIdMyGadgetTwentyFifteen.php';
-idmygadget_twentyfifteen_check_idMyGadget_install();
 $site_name = get_bloginfo('name' );
 $header_html = JmwsIdMyGadgetTwentyFifteen::getHeaderHtml();
 ?>
